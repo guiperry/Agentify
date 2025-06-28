3062bfd9e539d5adf6997662be530e115a5d474e
G <gperry@inc-line.com>
2025-06-27 20:45:30 -0400

    Add GitHub artifact download functionality for compiled plugins

--- netlify/functions/api-download-github-artifact-jobid.js
+++ netlify/functions/api-download-github-artifact-jobid.js
@@ -1,6 +1,6 @@
 // Auto-generated Netlify function from Next.js API route
 // Original route: /api/download/github-artifact/[jobId]
-// Generated: 2025-06-28T00:11:48.939Z
+// Generated: 2025-06-28T00:35:19.939Z
 
 // NextResponse/NextRequest converted to native Netlify response format
 const { createGitHubActionsCompiler } = require('./lib/github-actions-compiler.js');
